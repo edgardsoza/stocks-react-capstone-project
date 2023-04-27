0d11d8759d5fba8df8eb75d6acadbbda2adf3d4d
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-04-27 13:46:02 -0600

    fixing linter

--- src/test/Details.test.js
+++ src/test/Details.test.js
@@ -1,3 +1,4 @@
+import '@jest/globals';
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
--- src/test/fetchData.mock.js
+++ src/test/fetchData.mock.js
@@ -3,5 +3,5 @@ const fetchData = jest.fn(() => Promise.resolve([
   { date: '2022-01-01', netCashProvidedByOperatingActivities: 2000 },
   { date: '2023-01-01', netCashProvidedByOperatingActivities: 3000 },
 ]));
-  
+
 export default fetchData;
