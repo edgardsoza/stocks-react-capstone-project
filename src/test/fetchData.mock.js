07a01417536691a6506a012268368912c21783dc
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-04-27 13:41:10 -0600

    fix linter

--- src/test/fetchData.mock.js
+++ src/test/fetchData.mock.js
@@ -1,8 +1,7 @@
 const fetchData = jest.fn(() => Promise.resolve([
-    { date: '2021-01-01', netCashProvidedByOperatingActivities: 1000 },
-    { date: '2022-01-01', netCashProvidedByOperatingActivities: 2000 },
-    { date: '2023-01-01', netCashProvidedByOperatingActivities: 3000 },
-  ]));
+  { date: '2021-01-01', netCashProvidedByOperatingActivities: 1000 },
+  { date: '2022-01-01', netCashProvidedByOperatingActivities: 2000 },
+  { date: '2023-01-01', netCashProvidedByOperatingActivities: 3000 },
+]));
   
-  export default fetchData;
-  
+export default fetchData;
